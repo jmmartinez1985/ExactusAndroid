277ad7fa8c0720358344aa8e35b807f31a5a8e5b
jimmygk <yimmyperez@yahoo.com>
2015-10-09 21:32:26 -0500

    subiendo checheres

---
+++ app/src/main/java/exactus/jp/exactusjpapp/model/Cliente.java
@@ -0,0 +1,21 @@
+package exactus.jp.exactusjpapp.model;
+
+/**
+ * Created by James Perez on 09/10/2015.
+ */
+public class Cliente {
+
+
+    public String  CLIENTE ;
+    public String NOMBRE ;
+    public String CONTRIBUYENTE ;
+    public String ALIAS ;
+    public double SALDO ;
+    public double LIMITE_CREDITO ;
+    public String CATEGORIA_CLIENTE ;
+    public String NIVEL_PRECIO ;
+    public int CONDICION_PAGO ;
+    public int VERSION_PRECIO ;
+    public String EXENTO_IMPUESTOS ;
+
+}
