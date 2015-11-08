c6a4ed574b9191ed1967c960f9d88cc862fb839b
Jose Martinez <stn_rules@hotmail.com>
2015-11-08 13:04:15 -0500

    cambios

--- app/src/main/java/exactus/jp/exactusjpapp/LandingActivity.java
+++ app/src/main/java/exactus/jp/exactusjpapp/LandingActivity.java
@@ -3,9 +3,15 @@ package exactus.jp.exactusjpapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+
+import exactus.jp.exactusjpapp.adapter.MainFragmentPagerAdapter;
+import exactus.jp.exactusjpapp.adapter.PedidoFragmentPagerAdapter;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -14,16 +20,36 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("TRANSCT Mobile");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    /*    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        //Tabs + ViewPager
+        //Establecer el PageAdapter del componente ViewPager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        //viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager()));
+        viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.appbartabs);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
 
 }
